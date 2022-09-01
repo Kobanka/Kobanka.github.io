a894f9a2989a076496e118498c152ad72729c8ec
Aniss <anicetkobanka@gmail.com>
2022-09-01 01:59:09 +0100

    Initial commit

---
+++ _site/js/gallery.js
@@ -0,0 +1,37 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
+function activateGallery() {
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    let mainImage = document.querySelector("#gallery-photo img");
+  
+    thumbnails.forEach(function(thumbnail) {
+      thumbnail.addEventListener("click", function() {
+        // Preload large images.
+        let newImageSrc  = thumbnail.dataset.largeVersion;
+        let largeVersion = new Image();
+        largeVersion.src = newImageSrc;
+
+        // Set clicked image as main image.
+        let newImageAlt = thumbnail.dataset.description;
+        // Image info to be updated
+        let galleryInfo = document.querySelector("#gallery-info");
+        let title       = galleryInfo.querySelector(".title");
+        let description = galleryInfo.querySelector(".description");
+        let price       = galleryInfo.querySelector(".achat .prix");
+
+        mainImage.setAttribute("src", newImageSrc);
+        mainImage.setAttribute("alt", newImageAlt);
+
+        // Change which image is current.
+        let currentClass = "current";
+        document.querySelector(".current").classList.remove(currentClass);
+        thumbnail.parentNode.classList.add(currentClass);
+        // Update image info.
+        title.innerHTML       = thumbnail.dataset.title;
+        description.innerHTML = thumbnail.dataset.description;
+        price.innerHTML       = thumbnail.dataset.prix;
+
+      });
+    });
+  }
